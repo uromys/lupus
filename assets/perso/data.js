957c01284ab9c17ce06ca8302ce42b3737e9f1ad
Anael <lacour@mbp-de-anael.home>
2024-04-09 00:38:47 +0200

    add other button icon and better icon

--- assets/perso/data.js
+++ assets/perso/data.js
@@ -1,124 +1,145 @@
-
-const condition_Loup = "Pour gagner il doit tuer tout les membres du villages"
-const condition_village = "Pour gagner il doit tuer tout les loups garous"
+const condition_Loup = "Pour gagner il doit tuer tout les membres du villages";
+const condition_village = "Pour gagner il doit tuer tout les loups garous";
 export const perso = [
-    {
-        id: '122222222',
-        title: 'Loup garou',
-        pouvoir: 'Il se réunit chaque nuit avec les autres loups-garous pour décider de tuer un joueur',
-        photo: require("../loup-garou.jpg"),
-        description: 'Il connaît l\'identité des autres loups-garous et doit essayer de tuer tous les villageois sans se faire découvrir',
-        camp: "loup",
-        condition: condition_Loup
-    },
-    {
-        id: '1',
-        title: 'Loup ancien',
-        pouvoir: 'Lors de la première nuit, il peut infecter un membre du village, qui conserve ces pouvoirs mais doit désormais tuer tous les villageois',
-        photo: require("../loup-noir.jpg"),
-        description: 'Le loup ancien a traversé les âges et permis à la meute d\'atteindre un nombre important de membres',
-        camp: "loup",
-        condition: condition_Loup
-    },
-    {
-        id: '13332666',
-        title: 'Sbire',
-        photo: require("../sbire.webp"),
-        description: 'Serviteur et adorateurs des loups.' +
-            'Il ne se réveille pas en même temps que les loups et les loups garous ne connaissent pas son existence',
-        camp: "loup",
-        condition: condition_Loup
-    },
-    {
-        id: '8000',
-        title: 'Loup blanc',
-        photo: require("../loup-blanc.jpg"),
-        description: 'Le loup-garou blanc est un loup albinos, rejeté par ses pairs, il ne désire que se venger',
-        camp: "solo",
-        condition: 'doit être le dernier survivant du village ( avec potentiellement le survivant )'
-    },
-    {
-        id: '2',
-        title: 'Villageois',
-        photo: require("../villageois.jpg"),
-        description: 'Le villageois n\'a aucun pouvoir spécial, si ce n\'est de voter au conseil du village contre celui qu\'il suspecte être loup-garou.',
-        camp: "village",
-        condition: condition_village
-    },
-    {
-        id: '3',
-        title: 'Voyante',
-        pouvoir: 'Chaque nuit, elle peut connaître le rôle d\'un joueur qu\'elle aura choisi',
-        description: 'La voyante dispose d\'accès mystiques lui permettant de devenir reine du royaume des ombres',
-        camp: "village",
-        condition: condition_village
-    },
-    {
-        id: '4',
-        title: 'Chasseur',
-        description: 'Le chasseur n\'a aucun rôle particulier à jouer tant qu\'il est vivant. Mais dès qu\'il meurt, qu\'il soit tué dans la nuit (Loups-garous, sorcière) ou à la suite d\'une décision des villageois, il doit désigner une personne qui mourra également, sur-le-champ',
-        camp: "village",
-        condition: condition_village
-    },
-    {
-        id: '5',
-        title: 'Survivant',
-        photo: require("../survivant.jpeg"),
-        description: 'Le survivant n\'a pas de pouvoir. Son unique but est de rester en vie jusqu\'à la fin de la partie',
-        camp: "solo",
-        condition: "rester en vie jusqu'à la fin de la partie"
-    },
-    {
-        id: '6',
-        title: 'Ange',
-        photo: require("../ange.jpeg"),
-        description: 'Rageur, il ne cherche que la rédemption. Pour cela, il désire sacrifier son existence pour dévoiler la vérité.',
-        camp: "solo",
-        condition: "Gagnez seul lors du premier tour, ou gagnez avec le village"
-    },
-    {
-        id: '7',
-        title: 'Renard',
-        photo: require("../renard.jpg"),
-        description: 'Le renard a toujours eu un instinct lui permettant de déceler le vrai du faux',
-        pouvoir: 'Il désigne une personne, si la personne ou ses 2 voisins sont dans le camp des loups, alors il pourra réutiliser son pouvoir la nuit prochaine',
-        camp: "village",
-        condition: condition_village
-    },
-    {
-        id: '8',
-        title: 'Salvateur',
-        photo: require("../salvateur.jpg"),
-        description: 'Devenu maître des arts occultes,cet homme connait tout des loups garou',
-        pouvoir: 'Il designe une personne, cette personne est immunisé contre les loups garous pendant une nuit',
-        camp: "village",
-        condition: condition_village
-    },
-    {
-        id: '9',
-        title: 'Sorcière',
-        photo: require("../sorciere.jpg"),
-        description: "Experte en potion,on raconte qu'elle a traversé les âges",
-        pouvoir: "Elle dispose de deux potions à usage unique,une permet de ressusciter la victime des loups garoux, l'autres de tuer un joueur",
-        camp: "village",
-        condition: condition_village
-    },
-    {
-        id: '10',
-        title: 'Cupidon',
-        photo: require("../cupidon.jpg"),
-        description: "Ce chérubin ne désire que voir l'amour naître entre ces citoyens",
-        pouvoir: "Il designe deux joueurs, si l'un meurt l'autre meurt.Si leurs conditions de victoires sont en oppositions,ils gagnent lorsqu'ils sont les deux derniers en vie",
-        camp: "village",
-        condition: condition_village
-    },
-    {
-        id: '11',
-        title: "Montreur d'ours",
-        photo: require("../bear.png"),
-        description: "Bête de foire, cet homme a réussi à capturer un ours vivant qu'il a dressé pour faire des tours",
-        pouvoir: "Tout les matins,l'ours grognent si les voisins (ou si le montreurs d'ours suite à une infection par le loup noir ) sont dans le camps des loups",
-        camp: "village",
-        condition: condition_village
-    },
+  {
+    id: "122222222",
+    title: "Loup garou",
+    pouvoir:
+      "Il se réunit chaque nuit avec les autres loups-garous pour décider de tuer un joueur",
+    photo: require("../lupus.webp"),
+    description:
+      "Il connaît l'identité des autres loups-garous et doit essayer de tuer tous les villageois sans se faire découvrir",
+    camp: "loup",
+    condition: condition_Loup,
+  },
+  {
+    id: "1",
+    title: "Loup ancien",
+    pouvoir:
+      "Lors de la première nuit, il peut infecter un membre du village, qui conserve ces pouvoirs mais doit désormais tuer tous les villageois",
+    photo: require("../old_loup.webp"),
+    description:
+      "Le loup ancien a traversé les âges et permis à la meute d'atteindre un nombre important de membres",
+    camp: "loup",
+    condition: condition_Loup,
+  },
+  {
+    id: "13332666",
+    title: "Sbire",
+    photo: require("../sbire.webp"),
+    description:
+      "Serviteur et adorateurs des loups." +
+      "Il ne se réveille pas en même temps que les loups et les loups garous ne connaissent pas son existence",
+    camp: "loup",
+    condition: condition_Loup,
+  },
+  {
+    id: "8000",
+    title: "Loup blanc",
+    photo: require("../loup-blanc.jpg"),
+    description:
+      "Le loup-garou blanc est un loup albinos, rejeté par ses pairs, il ne désire que se venger",
+    camp: "solo",
+    condition:
+      "doit être le dernier survivant du village ( avec potentiellement le survivant )",
+  },
+  {
+    id: "2",
+    title: "Villageois",
+    photo: require("../villageois.jpg"),
+    description:
+      "Le villageois n'a aucun pouvoir spécial, si ce n'est de voter au conseil du village contre celui qu'il suspecte être loup-garou.",
+    camp: "village",
+    condition: condition_village,
+  },
+  {
+    id: "3",
+    title: "Voyante",
+    pouvoir:
+      "Chaque nuit, elle peut connaître le rôle d'un joueur qu'elle aura choisi",
+    description:
+      "La voyante dispose d'accès mystiques lui permettant de devenir reine du royaume des ombres",
+    camp: "village",
+    condition: condition_village,
+  },
+  {
+    id: "4",
+    title: "Chasseur",
+    description:
+      "Le chasseur n'a aucun rôle particulier à jouer tant qu'il est vivant. Mais dès qu'il meurt, qu'il soit tué dans la nuit (Loups-garous, sorcière) ou à la suite d'une décision des villageois, il doit désigner une personne qui mourra également, sur-le-champ",
+    camp: "village",
+    condition: condition_village,
+  },
+  {
+    id: "5",
+    title: "Survivant",
+    photo: require("../survivant.jpeg"),
+    description:
+      "Le survivant n'a pas de pouvoir. Son unique but est de rester en vie jusqu'à la fin de la partie",
+    camp: "solo",
+    condition: "rester en vie jusqu'à la fin de la partie",
+  },
+  {
+    id: "6",
+    title: "Ange",
+    photo: require("../ange.jpeg"),
+    description:
+      "Rageur, il ne cherche que la rédemption. Pour cela, il désire sacrifier son existence pour dévoiler la vérité.",
+    camp: "solo",
+    condition: "Gagnez seul lors du premier tour, ou gagnez avec le village",
+  },
+  {
+    id: "7",
+    title: "Renard",
+    photo: require("../renard.jpg"),
+    description:
+      "Le renard a toujours eu un instinct lui permettant de déceler le vrai du faux",
+    pouvoir:
+      "Il désigne une personne, si la personne ou ses 2 voisins sont dans le camp des loups, alors il pourra réutiliser son pouvoir la nuit prochaine",
+    camp: "village",
+    condition: condition_village,
+  },
+  {
+    id: "8",
+    title: "Salvateur",
+    photo: require("../salvateur.jpg"),
+    description:
+      "Devenu maître des arts occultes,cet homme connait tout des loups garou",
+    pouvoir:
+      "Il designe une personne, cette personne est immunisé contre les loups garous pendant une nuit",
+    camp: "village",
+    condition: condition_village,
+  },
+  {
+    id: "9",
+    title: "Sorcière",
+    photo: require("../sorciere.jpg"),
+    description: "Experte en potion,on raconte qu'elle a traversé les âges",
+    pouvoir:
+      "Elle dispose de deux potions à usage unique,une permet de ressusciter la victime des loups garoux, l'autres de tuer un joueur",
+    camp: "village",
+    condition: condition_village,
+  },
+  {
+    id: "10",
+    title: "Cupidon",
+    photo: require("../cupidon.jpg"),
+    description:
+      "Ce chérubin ne désire que voir l'amour naître entre ces citoyens",
+    pouvoir:
+      "Il designe deux joueurs, si l'un meurt l'autre meurt.Si leurs conditions de victoires sont en oppositions,ils gagnent lorsqu'ils sont les deux derniers en vie",
+    camp: "village",
+    condition: condition_village,
+  },
+  {
+    id: "11",
+    title: "Montreur d'ours",
+    photo: require("../bear.png"),
+    description:
+      "Bête de foire, cet homme a réussi à capturer un ours vivant qu'il a dressé pour faire des tours",
+    pouvoir:
+      "Tout les matins,l'ours grognent si les voisins (ou si le montreurs d'ours suite à une infection par le loup noir ) sont dans le camps des loups",
+    camp: "village",
+    condition: condition_village,
+  },
 ];
--- screens/Composition.js
+++ screens/Composition.js
@@ -1,178 +1,222 @@
-import React, { useState,useEffect } from 'react';
-import { View, Text, FlatList, Dimensions, StyleSheet, ScrollView } from 'react-native';
-import { Button, Card, IconButton, useTheme, Divider } from 'react-native-paper';
-import { perso } from '../assets/perso/data';
-import { usePersoContext } from '../context/PersoContext';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Creation from '../components/Creation';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  Dimensions,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import {
+  Button,
+  Card,
+  IconButton,
+  useTheme,
+  Divider,
+  Avatar,
+} from "react-native-paper";
+import { perso } from "../assets/perso/data";
+import { usePersoContext } from "../context/PersoContext";
+import Icon from "react-native-vector-icons/FontAwesome";
+import Creation from "../components/Creation";
 
 const Composition = () => {
-    const theme = useTheme();
-    const { inputList } = usePersoContext();
-    const [counts, setCounts] = useState({});
-    const { width } = Dimensions.get('window');
-    const cardWidth = width / 2 - 24;
-    const maxAllowedCount = inputList.length;
-    const [selectedCounts, setSelectedCounts] = useState({});
-    const [availablePlacesText, setAvailablePlacesText] = useState('');
-    useEffect(() => {
-        const totalSelectedCount = Object.values(selectedCounts).reduce((sum, count) => sum + count, 0);
-        const availablePlaces = maxAllowedCount - totalSelectedCount;
-        setAvailablePlacesText(`${availablePlaces} places disponibles`);
-    }, [selectedCounts, maxAllowedCount]);
-    const topNumberText = `${maxAllowedCount - Object.values(counts).reduce((sum, count) => sum + count, 0)} places disponibles`;
+  const theme = useTheme();
+  const { inputList } = usePersoContext();
+  const [counts, setCounts] = useState({});
+  const { width } = Dimensions.get("window");
+  const cardWidth = width / 2 - 24;
+  const maxAllowedCount = inputList.length;
+  const [selectedCounts, setSelectedCounts] = useState({});
+  const [availablePlacesText, setAvailablePlacesText] = useState("");
+  useEffect(() => {
+    const totalSelectedCount = Object.values(selectedCounts).reduce(
+      (sum, count) => sum + count,
+      0
+    );
+    const availablePlaces = maxAllowedCount - totalSelectedCount;
+    setAvailablePlacesText(`${availablePlaces} places disponibles`);
+  }, [selectedCounts, maxAllowedCount]);
+  const topNumberText = `${
+    maxAllowedCount -
+    Object.values(counts).reduce((sum, count) => sum + count, 0)
+  } places disponibles`;
 
-    const randomizeCounts = () => {
-        let totalAvailable = maxAllowedCount;
-        let newCounts = {};
-    
-        // Initialize all counts to zero
-        perso.forEach((p) => {
-            newCounts[p.id] = 0;
-        });
-    
-        // Distribute the counts randomly
-        while (totalAvailable > 0) {
-            perso.forEach((p) => {
-                if (totalAvailable > 0) {
-                    const randomIncrement = Math.floor(Math.random() * (totalAvailable + 1));
-                    newCounts[p.id] += randomIncrement;
-                    totalAvailable -= randomIncrement;
-                }
-            });
-        }
-    
-        setSelectedCounts(newCounts);
-    };
-    const handleIncrement = (id) => {
-        const currentTotalCount = Object.values(selectedCounts).reduce((sum, count) => sum + count, 0);
+  const allocateVillageoisAndOneLoup = () => {
+    let newCounts = {};
 
-        if (currentTotalCount < maxAllowedCount) {
-            setSelectedCounts((prevCounts) => {
-                const newCounts = {
-                    ...prevCounts,
-                    [id]: (prevCounts[id] || 0) + 1,
-                };
-                const persoTitle = perso.find((p) => p.id === id)?.title || "Unknown";
-                console.log(`Increment - ${persoTitle}: ${newCounts[id]}`); // Log the title and updated count
-                return newCounts;
-            });
-        }
-    };
+    perso.forEach((p) => {
+      if (p.title === "Villageois") {
+        newCounts[p.id] = maxAllowedCount - 1;
+      } else if (p.title === "Loup garou") {
+        newCounts[p.id] = 1;
+      } else {
+        newCounts[p.id] = 0;
+      }
+    });
+    setSelectedCounts(newCounts);
+  };
 
+  const randomizeCounts = () => {
+    let totalAvailable = maxAllowedCount;
+    let newCounts = {};
 
-    const handleDecrement = (id) => {
-        if (selectedCounts[id] > 0) {
-            setSelectedCounts((prevCounts) => {
-                const newCounts = {
-                    ...prevCounts,
-                    [id]: prevCounts[id] - 1,
-                };
-                const persoTitle = perso.find((p) => p.id === id)?.title || "Unknown";
-                console.log(`Decrement - ${persoTitle}: ${newCounts[id]}`); // Log the title and updated count
-                console.log("Selected Counts:", selectedCounts);
-                return newCounts;
-            });
-        }
-    };
-
-
-    const renderPerson = ({ item }) => {
-        return (
-            <View>
-                <Card style={styles.cardStyle}>
-                    <Card.Content style={styles.contentStyle}>
-                        <Text style={styles.titleStyle}>{item.title}</Text>
-
-                        <View style={styles.buttonContainerStyle}>
-                            <IconButton
-                                icon={() => <Icon name="arrow-up" size={20} color="black" />}
-                                onPress={() => handleIncrement(item.id)}
-                                style={styles.iconStyle}
-                            />
-                            <Text style={{ fontSize: 16, color: 'black' }}>{selectedCounts[item.id] || 0}</Text>
-                            <IconButton
-                                icon={() => <Icon name="arrow-down" size={20} color="black" />}
-                                onPress={() => handleDecrement(item.id)}
-                                style={styles.iconStyle}
-                            />
-                        </View>
-                    </Card.Content>
-                </Card>
-            </View>
-        );
-    };
-
-    const renderSublist = (camp) => {
-        const filteredList = perso.filter((item) => item.camp === camp);
-        return (
-            <View style={{ marginBottom: 16 }}>
-                <FlatList
-                    data={filteredList}
-                    renderItem={renderPerson}
-                    keyExtractor={(item) => item.id}
-                    numColumns={1}
-                />
-            </View>
-        );
-    };
-
-
-    const styles = StyleSheet.create({
-        cardStyle: {
-            width: cardWidth,
-            margin: 8,
-            borderRadius: 16,
-            overflow: 'hidden',
-            elevation: 4,
-        },
-        contentStyle: {
-            padding: 8,
-            backgroundColor: 'white',
-            borderRadius: 8,
-        },
-        titleStyle: {
-            fontSize: 10,
-            marginBottom: 12,
-            fontWeight: 'bold',
-            color: 'black',
-        },
-        buttonContainerStyle: {
-            flexDirection: 'row',
-           // alignItems: 'center',
-            //justifyContent: 'flex-start',
-            marginTop: 5,
-        },
-        iconStyle: {},
-        container: {
-            ...theme.container,
-            backgroundColor: theme.colors.primaryContainer,
-            padding: 16,
-        },
-        scrollViewContent: {
-            flexDirection: 'column', // Change to 'column'
-        },
-        column: {
-            marginBottom: 16,
-        },
+    perso.forEach((p) => {
+      newCounts[p.id] = 0;
     });
 
-    return (
-        <ScrollView
-            style={{ flex: 1 }}
-            contentContainerStyle={{ flexGrow: 1 }}
-        >
-            <Creation />
-            <Button onPress={randomizeCounts}>Randomize Counts</Button>
-            <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 16,color :'white' }}>{availablePlacesText}</Text>
-            <View style={styles.column}>{renderSublist('loup')}</View>
-            <Divider />
-            <View style={styles.column}>{renderSublist('village')}</View>
-            <Divider />
-            <View style={styles.column}>{renderSublist('solo')}</View>
+    while (totalAvailable > 0) {
+      perso.forEach((p) => {
+        if (totalAvailable > 0) {
+          const randomIncrement = Math.floor(
+            Math.random() * (totalAvailable + 1)
+          );
+          newCounts[p.id] += randomIncrement;
+          totalAvailable -= randomIncrement;
+        }
+      });
+    }
 
-        </ScrollView>
+    setSelectedCounts(newCounts);
+  };
+  const handleIncrement = (id) => {
+    const currentTotalCount = Object.values(selectedCounts).reduce(
+      (sum, count) => sum + count,
+      0
     );
+
+    if (currentTotalCount < maxAllowedCount) {
+      setSelectedCounts((prevCounts) => {
+        const newCounts = {
+          ...prevCounts,
+          [id]: (prevCounts[id] || 0) + 1,
+        };
+        const persoTitle = perso.find((p) => p.id === id)?.title || "Unknown";
+        console.log(`Increment - ${persoTitle}: ${newCounts[id]}`); // Log the title and updated count
+        return newCounts;
+      });
+    }
+  };
+
+  const handleDecrement = (id) => {
+    if (selectedCounts[id] > 0) {
+      setSelectedCounts((prevCounts) => {
+        const newCounts = {
+          ...prevCounts,
+          [id]: prevCounts[id] - 1,
+        };
+        const persoTitle = perso.find((p) => p.id === id)?.title || "Unknown";
+        console.log(`Decrement - ${persoTitle}: ${newCounts[id]}`); // Log the title and updated count
+        console.log("Selected Counts:", selectedCounts);
+        return newCounts;
+      });
+    }
+  };
+
+  const renderPerson = ({ item }) => {
+    return (
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <Avatar.Image size={100} source={item.photo} />
+        <Card style={styles.cardStyle}>
+          <Card.Content style={styles.contentStyle}>
+            <Text style={styles.titleStyle}>{item.title}</Text>
+
+            <View style={styles.buttonContainerStyle}>
+              <IconButton
+                icon={() => <Icon name="arrow-up" size={20} color="black" />}
+                onPress={() => handleIncrement(item.id)}
+                style={styles.iconStyle}
+              />
+              <Text style={{ fontSize: 16, color: "black" }}>
+                {selectedCounts[item.id] || 0}
+              </Text>
+              <IconButton
+                icon={() => <Icon name="arrow-down" size={20} color="black" />}
+                onPress={() => handleDecrement(item.id)}
+                style={styles.iconStyle}
+              />
+            </View>
+          </Card.Content>
+        </Card>
+      </View>
+    );
+  };
+
+  const renderSublist = (camp) => {
+    const filteredList = perso.filter((item) => item.camp === camp);
+    return (
+      <View style={{ marginBottom: 16 }}>
+        <FlatList
+          data={filteredList}
+          renderItem={renderPerson}
+          keyExtractor={(item) => item.id}
+          numColumns={1}
+        />
+      </View>
+    );
+  };
+
+  const styles = StyleSheet.create({
+    cardStyle: {
+      width: cardWidth,
+      margin: 8,
+      borderRadius: 16,
+      overflow: "hidden",
+      elevation: 4,
+    },
+    contentStyle: {
+      padding: 8,
+      backgroundColor: "white",
+      borderRadius: 8,
+    },
+    titleStyle: {
+      fontSize: 10,
+      marginBottom: 12,
+      fontWeight: "bold",
+      color: "black",
+    },
+    buttonContainerStyle: {
+      flexDirection: "row",
+      // alignItems: 'center',
+      //justifyContent: 'flex-start',
+      marginTop: 5,
+    },
+    iconStyle: {},
+    container: {
+      ...theme.container,
+      backgroundColor: theme.colors.primaryContainer,
+      padding: 16,
+    },
+    scrollViewContent: {
+      flexDirection: "column", // Change to 'column'
+    },
+    column: {
+      marginBottom: 16,
+    },
+  });
+
+  return (
+    <ScrollView style={{ flex: 1 }} contentContainerStyle={{ flexGrow: 1 }}>
+      <Creation />
+      <Button onPress={randomizeCounts}>Randomize Counts</Button>
+      <Button onPress={allocateVillageoisAndOneLoup}>One against all</Button>
+      <Text
+        style={{
+          fontSize: 18,
+          fontWeight: "bold",
+          marginBottom: 16,
+          color: "white",
+        }}
+      >
+        {availablePlacesText}
+      </Text>
+      <View style={styles.column}>{renderSublist("loup")}</View>
+      <Divider />
+      <View style={styles.column}>{renderSublist("village")}</View>
+      <Divider />
+      <View style={styles.column}>{renderSublist("solo")}</View>
+    </ScrollView>
+  );
 };
 
 export default Composition;
